3d592d88d6a14855499e6079461fb8c85a7a55c4
Akshit <103023426+akshit20421@users.noreply.github.com>
2024-11-08 23:26:27 +0530

    final changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,21 @@ function App() {
   return (
     <>
     <div className='bg'>
-    
+     <div className="mainImg"><img src="src\illustration-article.svg" alt="" />
+     </div>
+     
+     <div className="textMain">
+     <p className='tabColor'>Learning</p> </div>
+     <div className="pubMain"><p className='pubCol'>Published 7 Nov 2024</p>
+     </div>
+     <div className="headMain"><p className='headCol'>HTML & CSS foundations</p>
+     </div>
+     <div className="paraMain"><p className='paraCol'>These languages are the backbone of every website, defining structure, content, and presentation.</p>
+     </div>
+     <div className="nameMain">
+      <img src="src\image-avatar.webp" alt="" />
+      <p>Greg Hooper</p>
+     </div>
       </div>
     </>
   )
